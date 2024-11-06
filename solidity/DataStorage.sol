41fba381c5971e9603b5bcd17bcff731b20a0701
pengwenhao <pengwenhao@aliyun.com>
2024-11-06 18:59:56 +0800

    feat:init DataStorage.sol

---
+++ solidity/DataStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.21;
+
+contract  DataStrorage{
+
+
+function fCalldata(uint[] calldata _x) public pure returns(uint[] calldata){
+     return (_x);
+}
+
+}
+
