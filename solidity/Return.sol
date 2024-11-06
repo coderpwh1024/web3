d11bd259fd2e9144153d74918969446c67d5e8a5
pengwenhao <pengwenhao@aliyun.com>
2024-11-06 18:19:31 +0800

    fix:update return.sol

--- solidity/Return.sol
+++ solidity/Return.sol
@@ -18,6 +18,29 @@ contract Return{
        _array=[uint256(3),2,1];
    }
 
+
+   function returnNamed2() public pure returns(uint256 _number,bool _bool,uint256[3] memory _array ){
+    
+     return (1,true,[uint256(1),2,5]);
+   }
+
+   function readReturn() public pure{
+
+     uint256 _number;
+
+     bool _bool;
+
+     bool _bool12;
+
+     uint256[3] memory _array;
+
+     (_number,_bool,_array) =returnNamed();
+
+     (,_bool12,)=returnNamed();
+
+
+   }
+
  
 
 
